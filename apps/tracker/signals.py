afeb992718bcf3850dedc5adee35b6bed81fc136
crccheck <c@crccheck.com>
2018-07-18 11:08:59 -0500

    signals are sync

--- apps/tracker/signals.py
+++ apps/tracker/signals.py
@@ -5,6 +5,7 @@ track_point_added = Signal(providing_args=['product'])
 
 
 def update_product_pricing(sender, product, **kwargs):
+    import time; time.sleep(5)
     print('update_product_pricing', sender, product, kwargs)
 
 
