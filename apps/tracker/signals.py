b6b8bd7f25248ae67c024044277ad2de252efbcf
crccheck <c@crccheck.com>
2018-07-18 18:57:24 -0500

    POC of creating price differences

--- apps/tracker/signals.py
+++ apps/tracker/signals.py
@@ -1,3 +1,7 @@
+import datetime as dt
+
+from django.utils.timezone import now
+from django.db.models import Max, Q
 from django.dispatch import Signal
 
 
@@ -5,8 +9,14 @@ track_point_added = Signal(providing_args=['product'])
 
 
 def update_product_pricing(sender, product, **kwargs):
-    import time; time.sleep(5)
-    print('update_product_pricing', sender, product, kwargs)
+    latest_track = product.prices.latest('timestamp')
+    max_week = Max('price', filter=Q(timestamp__gte=now() - dt.timedelta(days=7)))
+    max_day = Max('price', filter=Q(timestamp__gte=now() - dt.timedelta(days=1)))
+    out = product.prices.aggregate(
+        week_diff=max_week - latest_track.price,
+        day_diff=max_day - latest_track.price,
+    )
+    print('update_product_pricing', latest_track, out)
 
 
 track_point_added.connect(update_product_pricing)
