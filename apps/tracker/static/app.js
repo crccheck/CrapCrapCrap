1bda4946a3f1cb56917eb33d100a0f058ba84b09
Chris Chang <c@crccheck.com>
2018-07-20 10:59:35 -0500

    Wishlist add/delete (#9)
    When displaying a product, you can now click the heart to add/delete it from the wishlist.
    
    Also trying to sneak in a tiny asset pipeline every PR because getting the whole thing set up is going to be such a pain

--- apps/tracker/static/app.js
+++ apps/tracker/static/app.js
@@ -1,33 +1,35 @@
 // https://docs.djangoproject.com/en/2.0/ref/csrf/#setting-the-token-on-the-ajax-request
-const csrftoken = document.cookie.match(/csrftoken=(\w+);/)[1];
+const csrftoken = document.cookie.match(/csrftoken=(\w+);/)[1]
 function csrfSafeMethod (method) {
   // these HTTP methods do not require CSRF protection
-  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method))
 }
 $.ajaxSetup({
   beforeSend: function (xhr, settings) {
     if (csrftoken && !csrfSafeMethod(settings.type) && !this.crossDomain) {
-      xhr.setRequestHeader('X-CSRFToken', csrftoken);
+      xhr.setRequestHeader('X-CSRFToken', csrftoken)
     }
-  }
-});
+  },
+})
 
 $('.product').each(function (idx, el) {
   const $product = $(el)
   $product.on('click', '.product--ui-wishlist', function (e, el) {
     e.preventDefault()
     const $el = $(this)
+    const method = $el.hasClass('active') ? 'DELETE' : 'PUT'
     $.ajax($el.attr('href'), {
-      method: 'PUT',
+      method,
       contentType: 'application/json',
       data: JSON.stringify({
         products: [$product.data('id')],
       }),
       success: (data) => {
         Object.keys(data).forEach((id) => {
-          $(`.product[data-id=${id}] .product--ui-wishlist`).addClass('active')
+          $(`.product[data-id=${id}] .product--ui-wishlist`)
+            .toggleClass('active', method === 'PUT')
         })
       },
     })
   })
-});
+})
