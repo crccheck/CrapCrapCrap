0a71eb13819459fafc0c54d29a338b854bf6842f
crccheck <c@crccheck.com>
2018-07-20 17:23:17 -0500

    Add dj debug toolbar

--- crap/urls.py
+++ crap/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
@@ -10,3 +11,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('receive/', csrf_exempt(ReceiverView.as_view())),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
