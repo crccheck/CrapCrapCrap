21b22db7599f0259b5d1e6e3bba54469071b7e77
crccheck <c@crccheck.com>
2018-07-20 09:36:09 -0500

    prep to send wishlist ajax call

--- apps/tracker/static/app.js
+++ apps/tracker/static/app.js
@@ -1,1 +1,22 @@
-console.log('hi')
+// https://docs.djangoproject.com/en/2.0/ref/csrf/#setting-the-token-on-the-ajax-request
+const csrftoken = document.cookie.match(/csrftoken=(\w+);/)[1];
+function csrfSafeMethod (method) {
+  // these HTTP methods do not require CSRF protection
+  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+  beforeSend: function (xhr, settings) {
+    if (csrftoken && !csrfSafeMethod(settings.type) && !this.crossDomain) {
+      xhr.setRequestHeader('X-CSRFToken', csrftoken);
+    }
+  }
+});
+
+$('.product').each(function (idx, el) {
+  const $product = $(el)
+  console.log($product.data('id'))
+  $product.on('click', '.product--ui-wishlist', function (e, el) {
+    e.preventDefault()
+    const $el = $(el)
+  })
+});
