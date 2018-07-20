7a04139c6c9d13af09a9257f86aa6e3dab23edd4
crccheck <c@crccheck.com>
2018-07-19 21:54:48 -0500

    only allow logged in users

--- apps/personalization/tests/test_urls.py
+++ apps/personalization/tests/test_urls.py
@@ -11,6 +11,7 @@ class ListToggleTests(TestCase):
         self.client = Client()
 
     def test_response(self):
-        ListFactory()
+        lis = ListFactory()
+        self.client.force_login(lis.owner)
         response = self.client.post(self.url)
         self.assertEqual(response.status_code, 200)
--- apps/personalization/urls.py
+++ apps/personalization/urls.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from . import views
 
 
 urlpatterns = [
-    path('api/list/toggle/', views.ListToggle.as_view(), name='list-toggle'),
+    path('api/list/toggle/', login_required(views.ListToggle.as_view()), name='list-toggle'),
 ]
