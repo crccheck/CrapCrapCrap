65d45e070bac819709d03793e683f28f5f9721ad
crccheck <c@crccheck.com>
2018-07-20 16:13:58 -0500

    add pagination to the template

--- crap/settings.py
+++ crap/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'django_cool_paginator',
 
     'apps.tracker',
     'apps.personalization',
