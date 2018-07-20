95ec687b1a7817b96827096acbf743d221a13183
crccheck <c@crccheck.com>
2018-07-20 11:55:25 -0500

    add pkgen helper

--- apps/personalization/models.py
+++ apps/personalization/models.py
@@ -1,5 +1,24 @@
+import random
+import string
+
 from django.conf import settings
 from django.db import models
+
+
+pkgen_candidates = string.ascii_letters + string.digits
+
+
+def pkgen(length=9):
+    """
+    Generate non-sequential primary keys that are also short enough.
+
+    Always start with a letter to avoid any potential int conversion. By
+    default, provides 52 * 62 ** 8 combinations 11,353,685,490,414,592.
+    """
+    ret = []
+    ret.append(random.choice(string.ascii_letters))
+    ret.extend([random.choice(pkgen_candidates) for __ in range(1, length)])
+    return ''.join(ret)
 
 
 class List(models.Model):
