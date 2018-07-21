444036d4c295334dfd36ff60beff9679a4dbba8f
crccheck <c@crccheck.com>
2018-07-20 22:15:58 -0500

    amazon ftw

--- crap/settings.py
+++ crap/settings.py
@@ -79,7 +79,6 @@ DATABASES = {
 # Auth
 ######
 
-SOCIAL_AUTH_POSTGRES_JSONFIELD = True
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -95,8 +94,22 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 AUTHENTICATION_BACKENDS = (
-    'social_core.backends.google.GoogleOpenId',
+    'social_core.backends.amazon.AmazonOAuth2',
     'django.contrib.auth.backends.ModelBackend',
+)
+SOCIAL_AUTH_AMAZON_KEY = env.require('SOCIAL_AUTH_AMAZON_KEY')
+SOCIAL_AUTH_AMAZON_SECRET = env.require('SOCIAL_AUTH_AMAZON_SECRET')
+SOCIAL_AUTH_POSTGRES_JSONFIELD = True
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    'social_core.pipeline.social_auth.associate_by_email',
 )
 
 # Internationalization
