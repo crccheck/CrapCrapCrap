0877084cba6ef3d174f05904862e64213034cd01
crccheck <c@crccheck.com>
2018-07-21 10:57:01 -0500

    first pass at parsing info

--- src/extension/amazon.js
+++ src/extension/amazon.js
@@ -1,3 +1,17 @@
+const { debug } = require('./utils')
+
 exports.amazon = function amazon () {
-  console.log('hi')
+  const aState = {}
+  document.querySelectorAll('script[type=a-state]').forEach((x) => {
+    const { key } = JSON.parse(x.dataset.aState)
+    aState[key] = JSON.parse(x.innerHTML)
+  })
+  // console.log('hi', )
+  debug('aState', aState)
+  const $price = document.querySelector('#_mediaPrice .value')
+  const price = $price.textContent.trim().replace(/[$,]/g, '')
+  const name = document.getElementById('productTitle').textContent.trim()
+  const identifier = aState['URL-Refresh-State'].landingAsin
+  const url = document.querySelector('link[rel=canonical]').href
+  return [{name, identifier, url, price}]
 }
