f3fdbf97f2a1fcbbcdd0a06cbdfe8f6664296918
crccheck <c@crccheck.com>
2018-09-03 01:35:55 -0500

    change popup content based on the response

--- browser_ext/popup/infobox.js
+++ browser_ext/popup/infobox.js
@@ -1,3 +1,8 @@
 console.log('popup hi -------------')
 
-browser.runtime.getBackgroundPage().then((bgWindow) => console.log(bgWindow.store))
+browser.runtime.getBackgroundPage().then((bgWindow) => {
+  console.log('popup', bgWindow.store)
+  const productCount = document.getElementById('placeholder--product-count')
+  console.log(productCount, bgWindow.store.payload)
+  productCount.innerHTML = bgWindow.store.payload.length
+})
--- src/extension/background.js
+++ src/extension/background.js
@@ -8,6 +8,7 @@ browser.runtime.onMessage.addListener(async (msg) => {
   // FIXME if another tab loads, it will use the current tab
   const [tab] = await browser.tabs.query({currentWindow: true, active: true})
   const { type, payload } = msg
+  window.store.payload = payload
   switch (type) {
     case 'data':
       const path = payload.length ? 'icons/ccc_loaded.svg' : 'icons/ccc_error.svg'
@@ -16,4 +17,7 @@ browser.runtime.onMessage.addListener(async (msg) => {
     default:
       console.log('Unknown message type: %s', type)
   }
+
+  // This doesn't work because the popup isn't open yet
+  // browser.extension.getViews({type: 'popup'})
 })
