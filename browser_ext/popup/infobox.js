9af17db49245c314f83f19b661bf411c9f518657
crccheck <c@crccheck.com>
2018-09-03 22:34:48 -0500

    handle plurals

--- browser_ext/popup/infobox.js
+++ browser_ext/popup/infobox.js
@@ -2,7 +2,8 @@ browser.runtime.getBackgroundPage().then((bgWindow) => {
   const { state } = bgWindow
   console.log('popup state:', state)
   const $productCount = document.getElementById('placeholder--product-count')
-  $productCount.innerHTML = state.payload.length
+  const plural = state.payload.length === 1 ? '' : 's'
+  $productCount.innerHTML = `${state.payload.length} product${plural} found!`
 
   const $wish = document.getElementById('placeholder--wish')
   if (state.shareUrl) {
