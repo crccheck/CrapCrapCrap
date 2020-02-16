101a24d5ef33a5537c3b210e95cb36d0f594bcde
crccheck <c@crccheck.com>
2020-02-16 00:12:24 -0600

    change scrapers from switch to map

--- src/extension/crap.js
+++ src/extension/crap.js
@@ -4,36 +4,28 @@ const { debug } = require('./utils')
 PriceData: { name: string, identifier: string, url: string, price: string }
 */
 
+const scrapers = new Map([
+  ['www.amazon.com', require('./stores/amazon').scrape],
+  ['www.bigbadtoystore.com', require('./stores/bbts').scrape],
+  ['www.wayfair.com', require('./stores/wayfair').scrape],
+])
 async function main () {
+  if (!scrapers.has(window.location.host)) {
+    debug('unknown host: %s', window.location.host)
+    return
+  }
+
+  const scraper = scrapers.get(window.location.host)
   let data/*: PriceData[] */ = []
-  switch (window.location.host) {
-    case 'www.amazon.com':
-      try {
-        data = require('./stores/amazon').scrape()
-      } catch (err) {
-        debug(err)
-      }
-      break
-    case 'www.bigbadtoystore.com':
-      try {
-        data = require('./stores/bbts').scrape()
-      } catch (err) {
-        debug(err)
-      }
-      break
-    case 'www.wayfair.com':
-      try {
-        data = require('./stores/wayfair').scrape()
-      } catch (err) {
-        debug(err)
-      }
-      break
-    default:
-      debug('unknown host: %s', window.location.host)
+  try {
+    data = scraper()
+  } catch (err) {
+    debug(err)
   }
   const response = await browser.runtime.sendMessage({ // eslint-disable-line no-unused-vars
     payload: data,
     referrer: window.location.href,
   })
 }
+
 setTimeout(main, 200 + Math.random() * 2000)
