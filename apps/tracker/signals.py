06b2b5be4e467699c20a43617fe8f28b844abb03
crccheck <c@crccheck.com>
2020-02-22 00:55:17 -0600

    use db to calc week too

--- apps/tracker/signals.py
+++ apps/tracker/signals.py
@@ -20,22 +20,23 @@ def update_product_pricing(sender, point: TrackPoint, **kwargs) -> None:
     product = point.product
     point.refresh_from_db()  # make sure `price` isn't a str
 
-    week_prices = product.prices.filter(timestamp__gte=week_ago).values_list('price', flat=True)
-    print(week_prices)
-    product.price_drop_short = max(week_prices) - point.price
-
-    max_ever = Max('price')
-    min_ever = Min('price')
     all_price_info = product.prices.aggregate(
-        count=Count('id'),
-        max_ever=max_ever,
-        min_ever=min_ever,
+        # count=Count('id'),
+        max_ever=Max('price'),
+        min_ever=Min('price'),
     )
     product.price_base = all_price_info["max_ever"]
     product.min_price = all_price_info["min_ever"]
     product.price_drop_long = product.price_base - point.price
     product.last_price = point.price
     product.last_price_check = point.timestamp
+
+    week_price_info = product.prices.filter(timestamp__gte=week_ago).aggregate(
+        # count=Count('id'),
+        max_week=Max('price'),
+    )
+    product.price_drop_short = week_price_info["max_week"] - point.price
+
 
     product.save()
 
