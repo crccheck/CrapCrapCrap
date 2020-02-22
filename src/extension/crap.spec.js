ed0c989749f6763d1778d2a116ff88e980eb4a68
crccheck <c@crccheck.com>
2020-02-21 23:30:50 -0600

    run crap.main sync

--- src/extension/crap.spec.js
+++ src/extension/crap.spec.js
@@ -4,8 +4,11 @@ global.window = {
   },
 }
 
+const crap = require('./crap')
+
+
 describe('extension js', () => {
-  it('loads with unknown host', () => {
-    require('./crap')
+  it('loads with unknown host', async () => {
+    await crap.main()
   })
 })
