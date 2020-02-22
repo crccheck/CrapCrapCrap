380be209917089a1053ab645b026f9696fc9c4ff
crccheck <c@crccheck.com>
2020-02-22 00:24:56 -0600

    add failing test case

--- apps/tracker/tests/test_signals.py
+++ apps/tracker/tests/test_signals.py
@@ -36,3 +36,21 @@ class TrackPointTests(TestCase):
         self.assertEqual(product.last_price, point.price)
         self.assertEqual(product.price_drop_short, max(0, prices[-2] - prices[-1]))
         self.assertEqual(product.price_drop_long, max(0, 1060 - prices[-1]))
+
+    def test_base_price_is_higest_seen_price(self):
+        product = ProductFactory()
+        TrackPointFactory(
+            product=product,
+            timestamp=timezone.now() - dt.timedelta(days=30),
+            price=100,
+        )
+        point = TrackPointFactory(
+            product=product,
+            timestamp=timezone.now() - dt.timedelta(days=1),
+            price=60,
+        )
+        track_point_added.send(sender=self, point=point)
+
+        self.assertEqual(product.price_base, 100)
+        self.assertEqual(product.min_price, 60)
+        self.assertEqual(product.last_price, 60)
